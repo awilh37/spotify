30a5a2fcc927e1c80a4250a2ce8c796f6d150717
Alex Wilhelm <awilh375@gmail.com>
2025-11-16 18:31:11 -0500

    update config.example.js - backendurl update

--- config.example.js
+++ config.example.js
@@ -1,2 +1,2 @@
 // Rename this file to config.js and replace the placeholder with your backend URL
-const backendUrl = 'YOUR_RENDER_BACKEND_URL';
+const backendUrl = 'https://spot_control.onrender.com';
